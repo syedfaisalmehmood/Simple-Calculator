f24afe17d084e828368cf00065a801c65cd26479
syedfaisalmehmood <syedfaisalmehmood@gmail.com>
2024-06-02 10:26:14 +0500

    Adding a Project of Simple Calculator.

--- index.ts
+++ index.ts
@@ -3,30 +3,29 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-    {message: "Enter your first number", type: "number", name: "firstnumber"},
-{message: "Enter  your second number", type: "number", name: "secondnumber"},
-{
+  { message: "Enter your first number", type: "number", name: "firstnumber" },
+  {
+    message: "Enter  your second number",
+    type: "number",
+    name: "secondnumber",
+  },
+  {
     message: "Select one of the operator to perform operations",
     type: "list",
     name: "operator",
     choices: ["Addition", "Multiplication", "Division", "Substraction"],
-}
-])
+  },
+]);
 
 // Conditional statement:
-if
-(answer.operator === "Addition") {
-    console.log(answer.firstnumber + answer.secondnumber);
+if (answer.operator === "Addition") {
+  console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstnumber / answer.secondnumber);
+} else if (answer.operator === "Substraction") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else {
+  console.log("Please input valid operator");
 }
-else if
-(answer.operator === "Multiplication") {
-    console.log(answer.firstnumber * answer.secondnumber);
-}
-else if(answer.operator === "Division") {
-    console.log(answer.firstnumber / answer.secondnumber);
-}
-else if(answer.operator === "Substraction") {
-    console.log(answer.firstnumber - answer.secondnumber);
-}
-
-else{console.log("Please input valid operator")}
